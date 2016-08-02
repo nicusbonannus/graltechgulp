5f3e7c0e95911eab9515f4e20c1bfb53d8924a34
Nicus Silver <Nicus Silver>
2016-08-02 16:16:46 -0300

    Agregue un git pull task

--- GraltechGulp/gulpfile.js
+++ GraltechGulp/gulpfile.js
@@ -3,8 +3,16 @@ This file in the main entry point for defining Gulp tasks and using Gulp plugins
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+     git = require('gulp-git');
 
 gulp.task('default', function () {
     console.log("Bonsoir Elliot");
 });
+
+gulp.task('gitpull', function () {
+    git.pull('origin', 'master', { args: '--rebase' }, function (err) {
+        if (err) throw err;
+    });
+});
+
