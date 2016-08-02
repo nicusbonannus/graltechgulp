c187bc98d16c46178660f0dda2153dae200cb472
Nicus Silver <Nicus Silver>
2016-08-02 13:58:42 -0300

    Agregue la solución inicial

---
+++ GraltechGulp/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace GraltechGulp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Dev()
+        {
+            return View();
+        }
+
+        // GET: Home
+        public ActionResult Prod()
+        {
+            return View();
+        }
+    }
+}
