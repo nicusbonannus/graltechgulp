436db4af65761593a4a89e583a292d195f43904f
Bernardo <b_muscio@hotmail.com>
2016-08-05 18:49:52 -0300

    Maurus y ber ejercicio

--- GraltechGulp/gulpfile.js
+++ GraltechGulp/gulpfile.js
@@ -5,7 +5,15 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 
 var gulp = require('gulp'),
      git = require('gulp-git'),
+     concat = require('gulp-concat'),
+     minimize = require('gulp-minify-css'),
+     uglify = require('gulp-uglify'),
+     watch = require('gulp-watch'),
     browserSync = require('browser-sync').create();
+
+var path = {
+    output: 'miStyle.css'
+};
 
 gulp.task('default', function () {
     console.log("https://www.youtube.com/channel/UC1DIYVPITelr6k_ahsblU1A");
@@ -23,4 +31,31 @@ gulp.task('reloadBrowser', ['gitpull'], function () {
     browserSync.reload({
         stream: true
     })
+});
+
+// gulp-concat
+// gulp-minify-css
+
+gulp.task('minimize', function () {
+    gulp.src('Content/**/*.css')
+    .pipe(concat('miStyle.css'))
+    .pipe(minimize())
+    .pipe(gulp.dest('styles'));
+});
+
+gulp.task('con-stream', function () {
+    //gulp.src('JavaScripts/*.js')
+});
+
+gulp.task('feificar', function () {
+    gulp.src('JavaScripts/**/*.js')
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('scripts'));
+});
+
+gulp.task('build', ['minimize', 'feificar']);
+
+gulp.task('watch', function () {
+    gulp.watch(['Content/**/*.css', 'JavaScripts/**/*.js'], ['build']);
 });
